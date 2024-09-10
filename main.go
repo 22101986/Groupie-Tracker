af0143672c02a8abdba81ea1afa8bbf89f4f2a9c
hpierrac <h.pierrache@gmail.com>
2024-09-10 11:00:44 +0200

    main.go

--- main.go
+++ main.go
@@ -30,29 +30,23 @@ func getArtistsData(url string) ([]Artist, error) {
 		return nil, err
 	}
 	defer resp.Body.Close()
-
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("erreur : statut de la réponse %d", resp.StatusCode)
 	}
-
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
-
 	var artists []Artist
 	err = json.Unmarshal(body, &artists)
 	if err != nil {
 		return nil, err
 	}
-
 	return artists, nil
 }
-
 func handlerIndex(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "templates/index.html", artists)
 }
-
 func handlerArtistDetail(w http.ResponseWriter, r *http.Request) {
 	// Extraire l'ID de l'URL manuellement
 	idStr := strings.TrimPrefix(r.URL.Path, "/artist/")
@@ -61,7 +55,6 @@ func handlerArtistDetail(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid artist ID", http.StatusBadRequest)
 		return
 	}
-
 	// Rechercher l'artiste par ID
 	for _, artist := range artists {
 		if artist.ID == id {
@@ -69,10 +62,8 @@ func handlerArtistDetail(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-
 	http.Error(w, "Artist not found", http.StatusNotFound)
 }
-
 func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 	t, err := template.ParseFiles(filepath.Join(tmpl))
 	if err != nil {
@@ -84,20 +75,16 @@ func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 		http.Error(w, "Template execution error", http.StatusInternalServerError)
 	}
 }
-
 func main() {
 	url := "https://groupietrackers.herokuapp.com/api/artists"
-
 	var err error
 	artists, err = getArtistsData(url)
 	if err != nil {
 		log.Fatalf("Erreur lors de la récupération des données des artistes: %v", err)
 	}
-
 	// Configurer les routes manuellement
 	http.HandleFunc("/", handlerIndex)
 	http.HandleFunc("/artist/", handlerArtistDetail) // Route pour les détails d'un artiste
-
 	srv := &http.Server{
 		Addr:              ":8443",
 		ReadHeaderTimeout: 10 * time.Second,
@@ -105,7 +92,6 @@ func main() {
 		IdleTimeout:       120 * time.Second,
 		MaxHeaderBytes:    1 << 20,
 	}
-
 	log.Println("Serveur démarré sur http://localhost:8443")
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err)
